90a3748b1bf777bbc644645b7906b0dd4492c092
updropbrasil <updropbrasil@gmail.com>
2026-07-08 20:41:14 -0300

    Delete jsapp directory

--- jsapp/app/robots.js
+++
@@ -1,9 +0,0 @@
-import { SITE_URL } from '../lib/config';
-
-export default function robots() {
-  const site = SITE_URL;
-  return {
-    rules: [{ userAgent: '*', allow: '/', disallow: ['/admin'] }],
-    sitemap: `${site}/sitemap.xml`
-  };
-}
